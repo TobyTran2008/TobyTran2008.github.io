8b513cb7d51079c53bb6106b65ff3bd341aa458f
Toby Tran <ttran26@siprep.org>
2024-02-08 19:51:29 -0800

    updated googleSheets.js code

--- resources/js/googleSheets.js
+++ resources/js/googleSheets.js
@@ -1,4 +1,39 @@
 function setUpGoogleSheets() {
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbxwqbUuJGFQ03tjE4ipOrgV24_jLUZFGG3RAjQsDwu_3iED2YdV2AVhnRrqcgyrB8Fx/exec';
+  const form = document.querySelector('#scoutingForm');
+  const btn = document.querySelector('#submit');
+
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+    btn.disabled = true;
+    btn.innerHTML = "Sending...";
+
+    let fd = getData(false);
+    for (const [key, value] of fd) {
+      console.log(`${key}: ${value}\n`);
+    }
+
+    fetch(scriptURL, { method: 'POST', body: fd })
+      .then(response => response.text())
+      .then(data => {
+        alert('Success!', data);
+      })
+      .catch(error => {
+        alert('Error!', error.message);
+      })
+      .finally(() => {
+        btn.disabled = false;
+        btn.innerHTML = "Send to Google Sheets";
+      });
+  });
+}
+
+
+/* 
+
+*****OLD CODE*****
+
+function setUpGoogleSheets() {
     const scriptURL = 'https://script.google.com/macros/s/AKfycbxwqbUuJGFQ03tjE4ipOrgV24_jLUZFGG3RAjQsDwu_3iED2YdV2AVhnRrqcgyrB8Fx/exec'
     const form = document.querySelector('#scoutingForm')
     const btn = document.querySelector('#submit')
@@ -14,7 +49,7 @@ function setUpGoogleSheets() {
         console.log(`${key}: ${value}\n`);
       }
 
-      fetch(scriptURL, { method: 'POST', mode: 'cors', body: fd })
+      fetch(scriptURL, { method: 'POST', body: fd })
         .then(response => { 
               alert('Success!', response) })
         .catch(error => {
@@ -24,3 +59,4 @@ function setUpGoogleSheets() {
       btn.innerHTML = "Send to Google Sheets"
     })
 }
+*/
