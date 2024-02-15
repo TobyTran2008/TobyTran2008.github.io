0303b3a02b5e761ca63611b19d3a4cd684456d8d
Toby Tran <ttran26@siprep.org>
2024-02-15 15:54:35 -0800

    changed dataFormat back to tsv bc I'm sad

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -1,7 +1,7 @@
 var config_data = `
 {
   "enable_google_sheets": "true",
-  "dataFormat": "kvs",
+  "dataFormat": "tsv",
   "title": "Scouting PASS 2024",
   "page_title": "Crescendo",
   "checkboxAs": "10",
