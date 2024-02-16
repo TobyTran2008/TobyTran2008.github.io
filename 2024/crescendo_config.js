e0f34870c5910055f7f14183bd1f756831ad6286
Toby Tran <ttran26@siprep.org>
2024-02-15 16:23:33 -0800

    change back to tsv bc I'm sad again

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -1,7 +1,7 @@
 var config_data = `
 {
   "enable_google_sheets": "true",
-  "dataFormat": "csv",
+  "dataFormat": "tsv",
   "title": "Scouting PASS 2024",
   "page_title": "Crescendo",
   "checkboxAs": "10",
