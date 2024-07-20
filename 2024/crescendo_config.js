9c0dd2075f9d38bd5f7b96f7fa35f4ce71a8ab2d
Toby Tran <ttran26@siprep.org>
2024-07-20 08:49:00 -0700

    changed event

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -18,7 +18,7 @@ var config_data = `
       "gsCol": "e",
       "code": "e",
       "type": "event",
-      "defaultValue": "2024OCR",
+      "defaultValue": "2024SSD",
       "required": "true"
     },
     { "name": "Match Level",
